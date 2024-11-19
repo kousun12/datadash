ddec3f70cce33954c4fb4a471a6e7dbeadf988b7
Rob Cheung <kousun12@gmail.com>
2024-11-19 10:58:26 -0500

    feat(framework): Add dynamic markdown file reader for chart definitions

---
+++ fw/src/d/[tableName]/[uuid].md.js
@@ -0,0 +1,20 @@
+import { fileURLToPath } from 'url';
+import path from "path";
+import fs from "fs";
+import {parseArgs} from "node:util";
+
+const {
+  values: {uuid, tableName}
+} = parseArgs({
+  options: {uuid: {type: "string"}, tableName: {type: "string"}},
+});
+
+
+function getMdText(uuid, tableName) {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const fp = path.join(__dirname, '../../../../chart_defs/sessions', tableName, uuid, "plot.md");
+  return fs.readFileSync(fp, 'utf8');
+}
+
+process.stdout.write(getMdText(uuid, tableName));
