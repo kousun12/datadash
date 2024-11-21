a8c7ee9faaab128783e14af5a7acf452c29d7e75
Rob Cheung <kousun12@gmail.com>
2024-11-20 19:57:12 -0500

    remove old sessions

--- chart_defs/sessions/4beb2033-a621-469d-822d-f53c17d5f4fe/query.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-  "Commodity Type",
-  "Marketing/calendar year" AS Year,
-  SUM(CAST("Value text" AS FLOAT)) AS TotalValue
-FROM ag_data
-WHERE "Marketing/calendar year" >= '2010/11'
-  AND Attribute IN ('Beginning stocks', 'Production', 'Imports')
-GROUP BY "Commodity Type", "Marketing/calendar year"
-ORDER BY "Commodity Type", "Marketing/calendar year"
